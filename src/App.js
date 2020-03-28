2b1405549588e421241274c2eec80338cb72696d
sriharsha <sriharsha.katragadda@devrabbit.com>
2020-03-28 09:38:11 -0700

    s

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import MyBio from './components/mybio'
-// import { j1 } from './utils2'
-import { j1 } from './utils'
+
 
 export default class App extends Component {
  
