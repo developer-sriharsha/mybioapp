5026da319bba65b72e2669aedf17ce0d948001cb
sriharsha <sriharsha.katragadda@devrabbit.com>
2020-03-30 01:44:08 -0700

    s

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import MyBio from './components/mybio'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+export default class App extends Component {
+ 
+  render() {
+    return (
+      <div>
+           <div className="App">
+     <MyBio/>
     </div>
-  );
+      </div>
+    )
+  }
 }
 
-export default App;
